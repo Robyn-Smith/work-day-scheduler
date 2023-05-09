8354cddf639abed6c27048eb69a065fccacde3fe
Robyn Smith <robynsmith21@hotmail.com>
2023-05-09 22:29:48 +0100

    removed past class inhtml in js removed uneed css

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var options = {
+var clock = {
     firstHour: 9,
     lastHour: 24,
 }
@@ -10,23 +10,23 @@ var currentHour = dayjs().hour();
 
 function onSaveToDo(event) {
 
-  var hour = $(event.target).parent().parent().attr('data-hour');
+  var hour = $(event.target).parent().parent().attr('time-hour');
   var toDo = $(event.target).parent().prev().children().val();
 
   localStorage.setItem(hour, toDo);
 
-  console.log('saved')
+  console.log('saved event');
 }
 
 function createBlock() {
 
-  for (hour = options.firstHour; hour <= options.lastHour; hour++) {
+  for (hour = clock.firstHour; hour <= clock.lastHour; hour++) {
 
     // load the toDo from local storage - jsn comment
     var savedToDo = localStorage.getItem(hour) || '';
-    var html = `<div class="row" data-hour = "${hour}">
+    var html = `<div class="row" time-hour = "${hour}">
         <div class="col-sm-2 hour"id="${hour}">${hour}</div>
-        <div class="col-sm-8 row past">
+        <div class="col-sm-8 row">
             <textarea class="col-md-10 description">${savedToDo}</textarea>
         </div>
         <div class="col-sm-2">
@@ -38,7 +38,7 @@ function createBlock() {
   }
 }
 
-//need to find data-hour class and loop through all checking if it is past present or future....................
+//need to find time-hour class and loop through all checking if it is past present or future
 function hourCheck() {
 $('.hour').each (function (){
   var test = parseInt($(this).attr("id"))
@@ -55,7 +55,7 @@ $('.hour').each (function (){
     $(this).addClass('future');
 console.log('hour check')
 }})}
-//need to add 'am' and 'pm' text after data-hour NOT WORKING .........................................
+//need to add 'am' and 'pm' text after time-hour
 
 function amPm () {
 $('.hour').each (function(index, element){
@@ -80,13 +80,13 @@ function init() {
     // updateTime();
     hourCheck();
 
-    amPm();//.........................................................not working..........................
+    amPm();
 
     //Set up the save button - jsn comment
     $('.saveBtn').on('click', onSaveToDo);
 
     //Set the current day - jsn comment
-    var currentDay = dayjs().format('dddd D MMMM YYYY, h:mm:ss a');
+    var currentDay = dayjs().format('dddd, D MMMM YYYY');
     $('#currentDay').text(currentDay);
 
     //Set up the Time poller - jsn comment
