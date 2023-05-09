ba502b210a31d22cffe99f97e66d81d083e1c377
Robyn Smith <robynsmith21@hotmail.com>
2023-05-09 18:32:12 +0100

    removed unnessary notes

--- assets/script.js
+++ assets/script.js
@@ -38,57 +38,8 @@ function createBlock() {
   }
 }
 
-// function updateTime() {
-
-//     console.log('updateTime');
-//     var currentHour = dayjs().hour();
-//     $('.time-block').each(function (index, element) {
-
-//       var hour = $(element).attr('data-hour');
-//       console.log(hour, currentHour);
-
-//       if(hour < currentHour) {
-//         $(element).find('.description').addClass('past');
-//       }
-//       else if (hour == currentHour) {
-//         $(element).find('.description').addClass('present');
-//       }
-//       else {
-//         $(element).find('.description').addClass('future');
-//       }
-//     });
-// }
-// bug in this section ..................................................................
-
-//add before 12 midday "am" and after 12 midday "pm".................................................
-
-// function hourCheck() {
-  // var currentHour = dayjs().hour(); 
-
-//   console.log('hourcheck 1', currentHour);
-
-//   $('.data-hour').each(function () {
-//     var timeBlockHour = parseInt($(this).attr('data-hour').split('data-hour')[1]);
-//     console.log('hourcheck 2');
-
-//     if (timeBlockHour < currentHour) {
-//       $(this).addClass('past');
-//     } else if (timeBlockHour === currentHour) {
-//       $(this).removeClass('past');
-//       $(this).addClass('present');
-//     } else {
-//       $(this).removeClass('past');
-//       $(this).removeClass('present');
-//       $(this).addClass('future');
-//     }
-//     console.log(timeBlockHour);
-//     console.log('hourCheck');
-//   });
-// }
-
 //need to find data-hour class and loop through all checking if it is past present or future....................
 function hourCheck() {
-// var test = parseInt($(this).attr('data-hour').split('data-hour')[1]);
 $('.hour').each (function (){
   var test = parseInt($(this).attr("id"))
   console.log(test)
@@ -121,17 +72,6 @@ $('.hour').each (function(index, element){
 })  
 }
 
-
-// var blockHour= $('<div>').attr('col-sm-2 hour')
-// var timeText = "";
-// if (hour >= 0 && hour <= 12) {
-//   timeText = "am";
-// }
-// else {
-//   timeText = "pm";
-// }
-// blockHour.text(hour + timeText);.............................................................
-
 function init() {
   //load in the time slots -jsn comment- change to - add in list
     createBlock();
@@ -157,26 +97,3 @@ function init() {
 }
 
 init();
-
-
-
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
