3e9275a59a41f4ea3fb77fe42d97b01567d9cf44
Robyn Smith <robynsmith21@hotmail.com>
2023-05-11 01:35:32 +0100

    adding notes to js removed unnessaryclock variable

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,16 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var clock = {
-    firstHour: 9,
-    lastHour: 17,
-}
+// All code that interacts with the DOM is wrapped in a call to jQuery to ensure that the code isn't run until 
+//the browser has finished rendering all the elements in the html.
 
 var currentHour = dayjs().hour();
-
-function onSaveToDo(event) {
-
-  var hour = $(event.target).parent().parent().attr('time-hour');
-  var toDo = $(event.target).parent().prev().children().val();
-
-  localStorage.setItem(hour, toDo);
-
-  console.log('saved event');
-}
+var firstHour = 0;
+var lastHour = 17;
+//global variables accessiable by all code
+//first hour set at 9:00am and last hour set at 17:00 to represent average office hours.
 
 function createBlock() {
+  for (hour = firstHour; hour <= lastHour; hour++) {
 
-  for (hour = clock.firstHour; hour <= clock.lastHour; hour++) {
-
-    // load the toDo from local storage - jsn comment
-    var savedToDo = localStorage.getItem(hour) || '';
+    var savedToDo = localStorage.getItem(hour) || '';   //this loads the plans entered by user from local storage
     var html = `<div class="row" time-hour = "${hour}">
         <div class="col-sm-2 hour"id="${hour}">${hour}</div>
         <div class="col-sm-8 row">
@@ -33,12 +20,25 @@ function createBlock() {
             <button class="btn btn-primary saveBtn">Save</button>
         </div>
       </div>`
-
       $('.container').append(html);
   }
 }
+//this function dynamically creates html and appends it to the div element in the index with the class name 
+//"container". This creates div elements comprising the office hours, the text boxes and the save buttons. 
+//This was achieved by the use of a for loop by starting at the first hour, 9:00 am and incrementing through 
+//the hours until all div elements are created for every hour leading up to 17:00pm, office hours.
+//Bootstrap is used with a class of row to add rows to the container and col-* classes for different column 
+//sizes on different screens.
 
-//need to find time-hour class and loop through all checking if it is past present or future
+function onSaveToDo(event) {
+  var hour = $(event.target).parent().parent().attr('time-hour');
+  var toDo = $(event.target).parent().prev().children().val();
+
+  localStorage.setItem(hour, toDo);
+  console.log('saved event');
+}
+//this function 
+
 function hourCheck() {
 $('.hour').each (function (){
   var test = parseInt($(this).attr("id"))
@@ -55,7 +55,7 @@ $('.hour').each (function (){
     $(this).addClass('future');
 console.log('hour check')
 }})}
-//need to add 'am' and 'pm' text after time-hour
+//need to find time-hour class and loop through all checking if it is past present or future
 
 function amPm () {
 $('.hour').each (function(index, element){
@@ -71,6 +71,7 @@ $('.hour').each (function(index, element){
   console.log('AM PM');
 })  
 }
+//need to add 'am' and 'pm' text after time-hour
 
 function init() {
   //load in the time slots -jsn comment- change to - add in list
